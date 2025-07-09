69a4f6ac68fd40635da0e498e9bbf4b82d87eef9
Chemlaliyed <iyedchemlal@gmail.com>
2025-07-09 16:10:42 +0100

    2nd commit

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from .models import User
+from . import forms
+class Signup(CreateView):
+    model=User
+    form_class=forms.Usersignupform
+    template_name='signup.html'
+    success_url='/'
+    def form_valid(self, form):
+        password=form.cleaned_data['password']
+        user=form.save()
+        user.set_password(password)
+        return super().form_valid(form)
 # Create your views here.
