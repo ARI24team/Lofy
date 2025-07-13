f1c1f5527328cd477f760bb81020d0f9e2f88a7d
reda <84156995+RedaArrous@users.noreply.github.com>
2025-07-13 13:22:40 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
-    Phone_Number=models.CharField(max_length=15,null=False,blank=True)
-    email=models.EmailField(null=False,blank=True)
+    Phone_Number=models.CharField(max_length=15,null=False)
+    email=models.EmailField(null=False)
 # Create your models here.
