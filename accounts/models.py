af68ed3a71ec8c3ecfbefba088100fa5311876d6
reda <hissocka2006@gmail.com>
2025-07-26 17:24:40 +0100

    removed an annoying comment

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,3 @@ class User(AbstractUser):
     Phone_Number=models.CharField(max_length=15,null=False, blank=True)
     email=models.EmailField(null=False, blank=True)
     email_confirmed = models.BooleanField(default=True)
-# Create your models here.
